507269c6c46a4a7703d9c029e4fd1934c7773fdf
m-schwanekamp <112090940+m-schwanekamp@users.noreply.github.com>
2022-10-25 14:14:45 -0400

    Update 8-2.cpp

--- 8-2.cpp
+++ 8-2.cpp
@@ -4,31 +4,34 @@
 #include <iomanip>
 
 using namespace std;
-int countVowel(string str) {
-    int count = 0;
-     while ((str) != '\0') {
-        if (str == 'a' || str == 'A' || str == 'e' || str == 'E' 
-        || str == 'i' || str == 'I' || str == 'o' || str == 'O'
-        || str == 'u' || str == 'U') {
-            count++;
-        }
-        str++;
-    }
-    return count;
-}
+int countVowel(string str);
 
-int main () {
+int main(void) {
    // string greeting = "Hello World!";
    // string sub = greeting.substr(0);
    // cout << sub << endl;
     string input;
     string str;
-    cout << "Enter a string or Q to quit: ";
-    cin >> input;
-    if (cin >> input) {
+    while (true) {
+        cout << "Enter a string or Q to quit: ";
+        getline(cin, input);
+        if (input == "Q") break;
+        countVowel(input);
         cout << "Vowel count: " << countVowel(str) << endl;
     }
-    else return 0;
-    }
+    return 0;
+}
 
+int countVowel(string str) {
+    int count;
+    while (str != "\0") {
+        if (str == "a" || str == "A" || str == "e" || str == "E"
+        || str == "i" || str == "I" || str == "o" || str == "O"
+        || str == "u" || str == "U") {
+            count++;
+        }
+    }
+    return count;
+}
+    
    
